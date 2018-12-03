50f78e51dbc6e384dbe5533ab53c12c363209668
Quentin <quentin.beaussart@gmail.com>
2018-12-03 08:06:14 +0100

    Main interface improvement

--- app/src/main/java/com/laethel/android/hardware_maintenance/controllers/MainActivity.java
+++ app/src/main/java/com/laethel/android/hardware_maintenance/controllers/MainActivity.java
@@ -2,6 +2,7 @@ package com.laethel.android.hardware_maintenance.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private Button mMainBt4;
     private Button mMainBt5;
     private Button mMainBt6;
+    private Button mMainBt7;
+    private Button mMainBt8;
+    private Button mMainBt9;
+    private Button mMainBt10;
     private Button mMainBtHome;
     private Button mMainBtRepairs;
     private Button mMainBtProfile;
@@ -36,8 +41,82 @@ public class MainActivity extends AppCompatActivity {
         mMainBt4 = (Button) findViewById(R.id.main_button4);
         mMainBt5 = (Button) findViewById(R.id.main_button5);
         mMainBt6 = (Button) findViewById(R.id.main_button6);
+        mMainBt7 = (Button) findViewById(R.id.main_button7);
+        mMainBt8 = (Button) findViewById(R.id.main_button8);
+        mMainBt9 = (Button) findViewById(R.id.main_button9);
+        mMainBt10 = (Button) findViewById(R.id.main_button10);
         mMainBtHome = (Button) findViewById(R.id.main_buttonHome);
         mMainBtRepairs = (Button) findViewById(R.id.main_buttonRepairs);
         mMainBtProfile = (Button) findViewById(R.id.main_buttonProfile);
+
+        mMainBt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mMainBt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mMainBt10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
